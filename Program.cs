4d4b870f69b5e9946610e6a87a0c7781eee124d4
Prodaku <b.isaacpro@gmail.com>
2018-12-17 16:29:04 +0100

    Fini

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sexe = "Monsieur";
+            string Prenom = "Vincent";
+            int visite = 1;
+
+            Console.WriteLine("Bonjour " + sexe + " " + Prenom + " vous êtes venu nous rendre visite " + visite + " fois aujourd'hui.");
+            
+        }
+    }
+}
